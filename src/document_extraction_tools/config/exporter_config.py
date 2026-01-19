c3d70a5ade3c6ac508106f4109d04d05b32220d2
Nikolas Moatsos <nikolas.moatsos@artefact.com>
2026-01-19 12:02:03 +0000

    Add destination field in exporter config

--- src/document_extraction_tools/config/exporter_config.py
+++ src/document_extraction_tools/config/exporter_config.py
@@ -1,7 +1,14 @@
 """Configuration for Exporter components."""
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+
+from document_extraction_tools.types.path_identifier import PathIdentifier
 
 
 class ExporterConfig(BaseModel):
     """Configuration for Exporters."""
+
+    destination: PathIdentifier = Field(
+        ...,
+        description="The root destination where exported files will be saved.",
+    )
